88b6fb7d0ce4f22298bb4016badc501e86e9f96d
SamGraber <samgraber@sundialproperties.com>
2016-05-05 13:25:39 -0400

    Installed toastr, which unfortunately requires jquery.

--- source/main.browser.ts
+++ source/main.browser.ts
@@ -1,5 +1,10 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import * as toastr from 'toastr';
 
 import { AppComponent } from './app.component';
+
+toastr.options = {
+	positionClass: 'toast-bottom-center',
+};
 
 bootstrap(AppComponent);
