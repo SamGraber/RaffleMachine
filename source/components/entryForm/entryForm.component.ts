5b0bce732a75380e7dee32b25079f0cd6abcbf8f
SamGraber <samgraber@sundialproperties.com>
2016-05-05 13:26:29 -0400

    Added a toastr notification for validation failures. Allow for submit on enter.

--- source/components/entryForm/entryForm.component.ts
+++ source/components/entryForm/entryForm.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/common';
 import { MdInput } from '@angular2-material/input';
 import { MdButton } from '@angular2-material/button';
+import { warning } from 'toastr';
 
 import { EntryService } from '../../services/entry/entry.service';
 
@@ -12,10 +14,15 @@ import { EntryService } from '../../services/entry/entry.service';
 })
 export class EntryFormComponent {
 	name: string;
+	@ViewChild('form') form: NgForm;
 	
 	constructor(private entryService: EntryService) {}
 
 	add(): void {
-		this.entryService.addEntry({ name: this.name });
+		if (this.form.valid) {
+			this.entryService.addEntry({ name: this.name });
+		} else {
+			warning('You must enter a name');
+		}
 	}
 }
