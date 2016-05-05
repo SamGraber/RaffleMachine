420547e2bc0e259a2b04b8b7fb55236d3ee0a88a
SamGraber <samgraber@sundialproperties.com>
2016-05-05 12:57:06 -0400

    Only show the list only if not empty.

--- source/components/entryList/entryList.component.ts
+++ source/components/entryList/entryList.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { Observable } from 'rxjs';
+import { isEmpty } from 'lodash';
 
 import { EntryService } from '../../services/entry/entry.service';
 
@@ -13,4 +15,8 @@ import { EntryService } from '../../services/entry/entry.service';
 })
 export class EntryListComponent {
 	constructor(public entryService: EntryService) { }
+	
+	notEmptyList(): Observable<boolean> {
+		return this.entryService.entryListChanges.map(x => !isEmpty(x));
+	}
 }
