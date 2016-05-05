64a7616cc5e853912c73d2e70974b29029e44f02
SamGraber <samgraber@sundialproperties.com>
2016-05-05 13:26:01 -0400

    Added the ability to remove entries.

--- source/components/entryList/entryList.component.ts
+++ source/components/entryList/entryList.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { MdButton } from '@angular2-material/button';
 import { Observable } from 'rxjs';
 import { isEmpty } from 'lodash';
 
-import { EntryService } from '../../services/entry/entry.service';
+import { EntryService, IEntry } from '../../services/entry/entry.service';
 
 @Component({
 	moduleId: module.id,
 	selector: 'rmEntryList',
 	templateUrl: 'entryList.component.html',
 	pipes: [AsyncPipe],
-	directives: [MD_LIST_DIRECTIVES],
+	directives: [MD_LIST_DIRECTIVES, MdButton],
 })
 export class EntryListComponent {
 	constructor(public entryService: EntryService) { }
 	
 	notEmptyList(): Observable<boolean> {
 		return this.entryService.entryListChanges.map(x => !isEmpty(x));
+	}
+	
+	remove(entry: IEntry): void {
+		this.entryService.removeEntry(entry);
 	}
 }
