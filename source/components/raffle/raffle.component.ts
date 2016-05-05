d6bf45f5c36cf07a844b88960056c34ca5a9dad9
SamGraber <samgraber@sundialproperties.com>
2016-05-05 14:23:13 -0400

    Created a component for enabling the raffle.

--- source/components/raffle/raffle.component.ts
+++ source/components/raffle/raffle.component.ts
@@ -1,16 +1,32 @@
 declare var module: any;
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { EntryService, IEntry } from '../../services/entry/entry.service';
+import { RandomService } from '../../services/random/random.service';
 
 @Component({
 	moduleId: module.id,
 	selector: 'rmRaffle',
-	templateUrl: 'raffle.component.html'
+	templateUrl: 'raffle.component.html',
+	pipes: [AsyncPipe],
 })
 export class RaffleComponent {
-	winner: IEntry;
+	winner: BehaviorSubject<IEntry>;
 	
-	constructor() {	}
+	constructor(private entryService: EntryService
+			, private randomService: RandomService) {	
+		this.winner = new BehaviorSubject<IEntry>(null);
+	}
 
+	raffle(): void {
+		this.entryService.entryListChanges.do(x => {
+			this.winner.next(this.randomService.random(x));
+		}).subscribe().unsubscribe();
+	}
+	
+	winnerName(): Observable<string> {
+		return this.winner.map(x => x != null ? x.name : null);
+	}
 }
